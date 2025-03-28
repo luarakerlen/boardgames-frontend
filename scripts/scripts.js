dbb18d37ee592ce332d46039f054c2119fb79cee
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2025-03-28 00:45:02 -0300

    adiciona funções para mostrar e esconder elementos na interface

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,13 @@
+export function showElement(element) {
+	element.classList.remove('hidden');
+	element.classList.add('visible');
+}
+
+export function hideElement(element) {
+	element.classList.remove('visible');
+	element.classList.add('hidden');
+}
+
 // Funções dos botões
 function scrollToTop() {
 	window.scrollTo({
