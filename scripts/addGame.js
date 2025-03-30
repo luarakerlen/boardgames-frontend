3082f3f470bd67265a000f5074a46127ae4c17be
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2025-03-30 01:45:56 -0300

    adiciona função para adicionar jogos ao servidor e refatora lógica de habilitação do botão

--- scripts/addGame.js
+++ scripts/addGame.js
@@ -1,4 +1,5 @@
 import { closeAddGameModal } from './modal.js';
+import { addGame } from './backend.js';
 
 const addGameButton = document.getElementById('addGameButton');
 
@@ -7,26 +8,49 @@ export const gameMinPlayersInput = document.querySelector('#gameMinPlayers');
 export const gameMaxPlayersInput = document.querySelector('#gameMaxPlayers');
 export const gameImageInput = document.querySelector('#gameImage');
 
-const isAddButtonDisabled =
-	!gameNameInput.value.trim() ||
-	!gameMinPlayersInput.value ||
-	!gameMaxPlayersInput.value;
+function isAddButtonDisabled() {
+	return (
+		!gameNameInput.value.trim() ||
+		!gameMinPlayersInput.value ||
+		!gameMaxPlayersInput.value
+	);
+}
 
-function handleAddGame(event) {
+async function handleAddGame(event) {
 	event.preventDefault();
 	closeAddGameModal();
 
-	Swal.fire({
-		title: 'Sucesso!',
-		text: 'O jogo foi adicionado à coleção.',
-		icon: 'success',
-		confirmButtonText: 'OK',
-		confirmButtonColor: '#007bff',
-	});
+	const formData = new FormData();
+	formData.append('name', gameNameInput.value);
+	formData.append('min_players', gameMinPlayersInput.value);
+	formData.append('max_players', gameMaxPlayersInput.value);
+	formData.append('image_url', gameImageInput.value);
+	console.log('formData', formData);
+
+	try {
+		const result = await addGame(formData);
+
+		Swal.fire({
+				title: 'Sucesso!',
+				text: 'O jogo foi adicionado à coleção.',
+				icon: 'success',
+				confirmButtonText: 'OK',
+				confirmButtonColor: '#007bff',
+			});
+	} catch (error) {
+		console.error('Erro ao adicionar jogo:', error.message || error);
+		Swal.fire({
+			title: 'Erro!',
+			text: `Ocorreu um erro ao adicionar o jogo: ${error.message || 'Erro desconhecido'}`,
+			icon: 'error',
+			confirmButtonText: 'OK',
+			confirmButtonColor: '#dc3545',
+		});
+	}
 }
 
 export function toggleAddButton() {
-	if (isAddButtonDisabled) {
+	if (isAddButtonDisabled()) {
 		addGameButton.setAttribute('disabled', 'true');
 	} else {
 		addGameButton.removeAttribute('disabled');
--- scripts/backend.js
+++ scripts/backend.js
@@ -1,33 +1,81 @@
 import { renderGamesList } from './gamesList.js';
 export const boardgames = [];
 
+const BASE_URL = 'http://127.0.0.1:5001';
+
 /*
   --------------------------------------------------------------------------------------
   Função para obter a lista de jogos de tabuleiro existentes do servidor via requisição GET
   --------------------------------------------------------------------------------------
 */
-const getList = async () => {
-	let url = 'http://127.0.0.1:5001/boardgames';
+async function getList() {
+	const url = `${BASE_URL}/boardgames`;
+
 	try {
 		const response = await fetch(url, { method: 'get' });
 		const data = await response.json();
 
-		boardgames.push(
-			...data.boardgames.map((game) => ({
-				id: game.id,
-				name: game.name,
-				minPlayers: game.min_players,
-				maxPlayers: game.max_players,
-				image: game.image_url ?? null,
-				description: game.description ?? null,
-				ludopediaURL: game.ludopedia_url ?? null,
-			}))
-		);
+		if (response.ok) {
+			boardgames.push(
+				...data.boardgames.map((game) => convertAPIResponseToGameObject(game))
+			);
 
-		renderGamesList(boardgames);
+			renderGamesList(boardgames);
+		}
 	} catch (error) {
 		console.error('Error:', error);
 	}
-};
+}
 
 getList();
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para adicionar um novo jogo de tabuleiro ao servidor via requisição POST
+  --------------------------------------------------------------------------------------
+*/
+
+export async function addGame(game) {
+	const url = `${BASE_URL}/boardgame`;
+	console.log('game', game);
+
+	try {
+		const response = await fetch(url, {
+			method: 'post',
+			headers: {
+				'Content-Type': 'multipart/form-data',
+			},
+			body: JSON.stringify(game),
+		});
+
+		if (!response.ok) {
+			const errorMessage = await response.text();
+
+			throw new Error(
+				JSON.parse(errorMessage).message ||
+					'Erro desconhecido ao adicionar o jogo'
+			);
+		}
+
+		const data = await response.json();
+		boardgames.push(convertAPIResponseToGameObject(data));
+		renderGamesList(boardgames);
+
+		return data;
+	} catch (error) {
+		console.error('Erro ao adicionar jogo no backend:', error.message || error);
+    throw error;
+	}
+}
+
+function convertAPIResponseToGameObject(game) {
+	return {
+		id: game.id,
+		name: game.name,
+		minPlayers: game.min_players,
+		maxPlayers: game.max_players,
+		image: game.image_url ?? null,
+		description: game.description ?? null,
+		ludopediaURL: game.ludopedia_url ?? null,
+	};
+}
